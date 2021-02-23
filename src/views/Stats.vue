28ad222f92202990f282a2945564c8fb7a14e456
Benjent <Benjent@hotmail.fr>
2021-02-23 21:22:33 +0100

    Lint a bit Stats page + correct baker gurits army format

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,21 +1,21 @@
 <template>
-    <section class="l-stats">
-        <section class="l-stats__numbers">
-            <span class="l-stats__number">{{ artists.length }} artists</span>
-            <span class="l-stats__number">{{ albums.length }} albums</span>
-            <span class="l-stats__number">{{ gemsNb }} gems</span>
-            <span class="l-stats__number">{{ subgenres.length }} subgenres</span>
+    <section class="stats">
+        <section class="stats__numbers">
+            <span class="stats__number">{{ artists.length }} artists</span>
+            <span class="stats__number">{{ albums.length }} albums</span>
+            <span class="stats__number">{{ gemsNb }} gems</span>
+            <span class="stats__number">{{ subgenres.length }} subgenres</span>
         </section>
         <!-- <hr> -->
-        <histogram-vertical class="l-stats__item" caption="Number of albums per year" :datasource="albumsPerYearWithRatio"></histogram-vertical>
+        <histogram-vertical class="stats__item" caption="Number of albums per year" :datasource="albumsPerYearWithRatio"></histogram-vertical>
         <!-- <hr> -->
-        <histogram-horizontal class="l-stats__item" caption="Number of albums per region" :datasource="albumsPerCountryWithRatio"></histogram-horizontal>
+        <histogram-horizontal class="stats__item" caption="Number of albums per region" :datasource="albumsPerCountryWithRatio"></histogram-horizontal>
         <!-- <hr> -->
-        <histogram-horizontal class="l-stats__item" caption="Artists with most gems" :datasource="artistsWithGemsWithRatio"></histogram-horizontal>
+        <histogram-horizontal class="stats__item" caption="Artists with most gems" :datasource="artistsWithGemsWithRatio"></histogram-horizontal>
         <!-- <hr> -->
-        <histogram-horizontal class="l-stats__item" caption="Artists with most albums" :datasource="artistsWithAlbumsWithRatio"></histogram-horizontal>
+        <histogram-horizontal class="stats__item" caption="Artists with most albums" :datasource="artistsWithAlbumsWithRatio"></histogram-horizontal>
         <!-- <hr> -->
-        <histogram-horizontal class="l-stats__item" caption="Greatest criteria occurences" :datasource="criteriaOccurencesWithRatio"></histogram-horizontal>
+        <histogram-horizontal class="stats__item" caption="Greatest criteria occurences" :datasource="criteriaOccurencesWithRatio"></histogram-horizontal>
     </section>
 </template>
 
@@ -53,91 +53,91 @@ export default {
         },
         albumsPerCountryWithRatio() {
     
-            const obj = this.albumsPerCountry;
-            const arr = Object.keys( obj ).map(function ( key ) { return obj[key]; });
-            const max = Math.max.apply( null, arr );
-            const min = 0;
+            const obj = this.albumsPerCountry
+            const arr = Object.keys(obj).map((key) => obj[key])
+            const max = Math.max.apply(null, arr)
+            const min = 0
         
             const albumsPerCountryWithRatio = []
         
             for(const country in this.albumsPerCountry) {
         
-                const nbOfAlbums = this.albumsPerCountry[country];
-                const ratio = (nbOfAlbums - min) / (max - min);
+                const nbOfAlbums = this.albumsPerCountry[country]
+                const ratio = (nbOfAlbums - min) / (max - min)
         
                 albumsPerCountryWithRatio.push({
                     label: country,
                     data: nbOfAlbums, 
                     ratio: ratio,
                     ratioPercent: (ratio * 100).toString() + '%',
-                });
-            };
+                })
+            }
     
             // Sort in DESC
-            albumsPerCountryWithRatio.sort(function(a, b){
+            albumsPerCountryWithRatio.sort((a, b) => {
                 const countryA = a.data
-                const countryB = b.data;
+                const countryB = b.data
         
                 if (countryA > countryB) {
-                return -1;
+                    return -1
                 }
                 if (countryA < countryB) {
-                return 1;
+                    return 1
                 }
                 return 0 // Default return value (no sorting)
             })
     
-          return albumsPerCountryWithRatio;
+          return albumsPerCountryWithRatio
         },
         criteriaOccurencesWithRatio() {
     
             const obj = this.criteriaOccurences;
-            const arr = Object.keys( obj ).map(function ( key ) { return obj[key]; });
-            const max = Math.max.apply( null, arr );
-            const min = 0;
+            const arr = Object.keys(obj).map((key) => obj[key])
+            const max = Math.max.apply(null, arr)
+            const min = 0
         
-            const criteriaOccurencesWithRatio = [];
+            const criteriaOccurencesWithRatio = []
 
-            for(const criterium in this.criteriaOccurences) {
+            for (const criterium in this.criteriaOccurences) {
 
-                const nbOfOccurences = this.criteriaOccurences[criterium];
-                const ratio = (nbOfOccurences - min) / (max - min);
+                const nbOfOccurences = this.criteriaOccurences[criterium]
+                const ratio = (nbOfOccurences - min) / (max - min)
         
                 criteriaOccurencesWithRatio.push({
                     label: this.$options.filters.criterium(criterium),
                     data: nbOfOccurences, 
                     ratio: ratio,
                     ratioPercent: (ratio * 100).toString() + '%',
-                });
-            };
+                })
+            }
 
             // Sort in DESC
-            criteriaOccurencesWithRatio.sort(function(a, b){
+            criteriaOccurencesWithRatio.sort((a, b) => {
                 const criteriumA = a.data
-                const criteriumB = b.data;
+                const criteriumB = b.data
         
                 if (criteriumA > criteriumB) {
-                return -1;
+                    return -1
                 }
                 if (criteriumA < criteriumB) {
-                return 1;
+                    return 1
                 }
                 return 0 // Default return value (no sorting)
             })
     
-          return criteriaOccurencesWithRatio;
+          return criteriaOccurencesWithRatio
         },
 		artistsWithGemsWithRatio() {
 			const min = 2
-			const artistsWithSeveralGems = Object.entries(this.artistsWithMostGems).filter(item => item[1] >= min)
+			const artistsWithSeveralGems = Object.entries(this.artistsWithMostGems).filter((item) => item[1] >= min)
 			artistsWithSeveralGems.sort((a, b) => b[1] - a[1])
 			const max = artistsWithSeveralGems[0][1] // Since it is sorted DESC
 			
 
 			const artistsWithGemsWithRatio = []
-			artistsWithSeveralGems.forEach(artist => {
-				const nbOfGems = artist[1];
-                const ratio = (nbOfGems - min) / (max - min);
+			artistsWithSeveralGems.forEach((artist) => {
+				const nbOfGems = artist[1]
+                const ratio = (nbOfGems - min) / (max - min)
 
                 artistsWithGemsWithRatio.push({
                     label: artist[0],
@@ -151,14 +151,14 @@ export default {
 		},
 		artistsWithAlbumsWithRatio() {
 			const min = 8
-			const artistsWithSeveralAlbums = Object.entries(this.artistsWithMostAlbums).filter(item => item[1] >= min)
+			const artistsWithSeveralAlbums = Object.entries(this.artistsWithMostAlbums).filter((item) => item[1] >= min)
 			artistsWithSeveralAlbums.sort((a, b) => b[1] - a[1])
 			const max = artistsWithSeveralAlbums[0][1] // Since it is sorted DESC
 
 			const artistsWithAlbumsWithRatio = []
 			artistsWithSeveralAlbums.forEach(artist => {
-				const nbOfAlbums = artist[1];
-                const ratio = (nbOfAlbums - min) / (max - min);
+				const nbOfAlbums = artist[1]
+                const ratio = (nbOfAlbums - min) / (max - min)
 
                 artistsWithAlbumsWithRatio.push({
                     label: artist[0],
@@ -177,7 +177,7 @@ export default {
 <style lang="scss" scoped>
 @import '../style/gatherer';
 
-.l-stats {
+.stats {
     padding: 60px 160px;
 
     & &__numbers {
@@ -192,7 +192,7 @@ export default {
 }
 
 @media (max-width: $mobile) {
-    .l-stats {
+    .stats {
         padding: 20px;
 
         & &__numbers {
