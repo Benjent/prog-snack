a5118c4b6cdc49580c7a6a740df45a7f542b529c
Benjent <Benjent@hotmail.fr>
2021-12-23 09:31:26 +0100

    Add last two pesnyary albums

--- src/db/albums/Д.js
+++ src/db/albums/Д.js
@@ -2,28 +2,64 @@ import regions from '../regions'
 import { criteria } from '../criteria'
 
 const albums = [
-    // TODO importer ce fichier et les autres langues étrangères dans le fichier index.js
-    // { // TODO c'est un peu complique mais ça reste prog les deux derniers albms
-        // id: 'pesnyary_pesnyary',
-        // title: 'Песняры',
-        // artist: 'Песняры',
-        // year: 1971,
-        // country: regions.RUSSIA,
-        // cover: '.jpg',
-        // criteria: [criteria.RUSSIAN, criteria.ROCK, criteria.C_FOLK],
-        // selectedTrackTitle: 'todo', // Рушники
-        // selectedTrackYtId: 'todo',
-        // spotifyId: 'todo',
-        // deezerId: 'todo',
-        // designers: [],
-        // similarAlbums: [],
-        // isAGem: false,
-    // },
-    // TODO le deuxième : baroque, russian
-    // TODO III : fanfare
-    // track is Крик Птицы
-    // TODO IV - uncommon, c_folk, baroque, odd, fanfare
-    // track Перепелочка
+    { // TODO moyen prog
+        id: 'pesnyary_pesnyary',
+        title: 'Песняры',
+        artist: 'Песняры',
+        year: 1971,
+        country: regions.RUSSIA,
+        cover: '.jpg',
+        criteria: [criteria.RUSSIAN, criteria.ROCK, criteria.C_FOLK],
+        selectedTrackTitle: 'todo', // Рушники
+        selectedTrackYtId: 'todo',
+        spotifyId: null,
+        deezerId: null,
+        designers: [],
+        similarAlbums: [],
+        isAGem: false,
+    },
+    // TODO le deuxième : baroque, russian - 1972  // TODO moyen prog
+    {
+        id: 'pesnyary_pesnyary_iii',
+        title: 'Песняры III',
+        artist: 'Песняры',
+        year: 1974,
+        country: regions.RUSSIA,
+        cover: '.jpg',
+        criteria: [criteria.RUSSIAN, criteria.FANFARE],
+        selectedTrackTitle: 'Крик Птицы',
+        selectedTrackYtId: 'HIf0Rev1xRE',
+        spotifyId: null,
+        deezerId: null,
+        designers: [],
+        similarAlbums: [],
+        isAGem: false,
+    },
+    {
+        id: 'pesnyary_pesnyary_iv',
+        title: 'Песняры IV',
+        artist: 'Песняры',
+        year: 1978,
+        country: regions.RUSSIA,
+        cover: '.jpg',
+        criteria: [criteria.RUSSIAN, criteria.FANFARE, criteria.UNCOMMON_INSTRUMENTS, criteria.C_FOLK, criteria.BAROQUE, criteria.ODD_RHYTHM],
+        selectedTrackTitle: 'Перепелочка',
+        selectedTrackYtId: '9H2bgx4mBGw',
+        spotifyId: null,
+        deezerId: null,
+        designers: [],
+        similarAlbums: [],
+        isAGem: false,
+    },
+    // TODO ФСБ BULGARIA - Нон Стоп - 1977
+    // c_electro, bulgarian, odd, criteria.SYMPHONIC, instru, jazzygroovy
+    // designers Евгени Донков
+    // track is Интермецо
+    // TODO ФСБ BULGARIA - ФСБ II - 1978
+    // c_electro, odd, symphonic, instru?, c_jazz
+    // track Утро
+    // designers Николай Пекарев
+    // gem true !!
 ]
 
 export default albums
