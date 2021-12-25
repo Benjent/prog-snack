c81f9e09c4286362563ab1284bf0e59d26a5102e
Benjent <Benjent@hotmail.fr>
2021-12-25 11:48:23 +0100

    Add fsb albums

--- src/db/albums/Д.js
+++ src/db/albums/Д.js
@@ -2,23 +2,6 @@ import regions from '../regions'
 import { criteria } from '../criteria'
 
 const albums = [
-    { // TODO moyen prog
-        id: 'pesnyary_pesnyary',
-        title: 'Песняры',
-        artist: 'Песняры',
-        year: 1971,
-        country: regions.RUSSIA,
-        cover: '.jpg',
-        criteria: [criteria.RUSSIAN, criteria.ROCK, criteria.C_FOLK],
-        selectedTrackTitle: 'todo', // Рушники
-        selectedTrackYtId: 'todo',
-        spotifyId: null,
-        deezerId: null,
-        designers: [],
-        similarAlbums: [],
-        isAGem: false,
-    },
-    // TODO le deuxième : baroque, russian - 1972  // TODO moyen prog
     {
         id: 'pesnyary_pesnyary_iii',
         title: 'Песняры III',
@@ -26,7 +9,7 @@ const albums = [
         year: 1974,
         country: regions.RUSSIA,
         cover: '.jpg',
-        criteria: [criteria.RUSSIAN, criteria.FANFARE],
+        criteria: [criteria.BELARUSIAN, criteria.FANFARE],
         selectedTrackTitle: 'Крик Птицы',
         selectedTrackYtId: 'HIf0Rev1xRE',
         spotifyId: null,
@@ -51,15 +34,39 @@ const albums = [
         similarAlbums: [],
         isAGem: false,
     },
-    // TODO ФСБ BULGARIA - Нон Стоп - 1977
-    // c_electro, bulgarian, odd, criteria.SYMPHONIC, instru, jazzygroovy
-    // designers Евгени Донков
-    // track is Интермецо
-    // TODO ФСБ BULGARIA - ФСБ II - 1978
-    // c_electro, odd, symphonic, instru?, c_jazz
-    // track Утро
-    // designers Николай Пекарев
-    // gem true !!
+    {
+        id: 'fsb_non_stop',
+        title: 'Нон Стоп',
+        artist: 'ФСБ',
+        year: 1977,
+        country: regions.BULGARIA,
+        cover: '.jpg',
+        criteria: [criteria.C_ELECTRO, criteria.BULGARIAN, criteria.ODD_RHYTHM, criteria.SYMPHONIC, criteria.INSTRUMENTAL, criteria.JAZZY_GROOVY],
+        selectedTrackTitle: 'Интермецо',
+        selectedTrackYtId: 'p11H1lXabIY',
+        spotifyId: null,
+        deezerId: null,
+        designers: ['Евгени Донков'],
+        similarAlbums: [],
+        isAGem: false,
+    },
+    {
+        id: 'fsb_fsb_ii',
+        title: 'ФСБ II',
+        artist: 'ФСБ',
+        year: 1978,
+        country: regions.BULGARIA,
+        cover: '.jpg',
+        criteria: [criteria.C_ELECTRO, criteria.ODD_RHYTHM, criteria.SYMPHONIC, criteria.INSTRUMENTAL, criteria.C_JAZZ],
+        selectedTrackTitle: 'Утро',
+        selectedTrackYtId: 'JSi6Iud7Z4U',
+        spotifyId: null,
+        deezerId: null,
+        designers: ['Николай Пекарев'],
+        similarAlbums: [],
+        isAGem: true,
+        description: `TODO Description`,
+    },
 ]
 
 export default albums
