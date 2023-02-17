38e12ed08caad8edccd6b79f0d791034d3b7e1ea
Benjent <Benjent@hotmail.fr>
2023-02-17 09:20:59 +0100

    refactor(criteria): use index to export

---
+++ src/db/criteria/index.js
@@ -0,0 +1,10 @@
+import { criteria, criteriaOrder } from "./criteria"
+import { categories, categoriesOrder, criteriaCategory } from "./categories"
+
+export {
+    categories,
+    categoriesOrder,
+    criteria,
+    criteriaCategory,
+    criteriaOrder,
+}
