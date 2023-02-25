57f67f1a14941a5e892d9d90ba8c378a0fb96519
Benjent <Benjent@hotmail.fr>
2023-02-25 10:17:21 +0100

    feat(package): add imagemin webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const ImageminPlugin = require("imagemin-webpack-plugin").ImageminPlugin
 
 module.exports = {
     lintOnSave: false,
     publicPath: "/prog-snack/",
     configureWebpack: {
         plugins: [
+            new ImageminPlugin({
+                disable: process.env.NODE_ENV !== "production",
+                test: "/assets/img/covers/**",
+            }),
             new BundleAnalyzerPlugin({ analyzerPort: 8081 }),
         ],
     },
