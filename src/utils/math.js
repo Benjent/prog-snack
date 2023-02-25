5d1972ac61d525cad2fe72f4647734d1553ef101
Benjent <Benjent@hotmail.fr>
2023-02-25 12:31:40 +0100

    docs(utils): add jsdoc and notify readme

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,6 +1,11 @@
+/**
+ * Get a random integer.
+ * @param {max} array - The maximum possible integer.
+ * @return {number} A random integer.
+ */
 const getRandomNumber = (max) => {
     const min = 0
-    return Math.floor(Math.random() * (max - min)) + min // Randomize id
+    return Math.floor(Math.random() * (max - min)) + min
 }
 
 export {
