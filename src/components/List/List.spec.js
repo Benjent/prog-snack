682488bb7be448cd8dd88357a5afe39a31a44bc0
Benjent <Benjent@hotmail.fr>
2023-02-27 00:33:51 +0100

    test(List): add tests

---
+++ src/components/List/List.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount } from "@vue/test-utils"
+import { List } from "@/components"
+
+describe("List.vue", () => {
+    let wrapper
+    beforeEach(() => {
+        const propsData = {
+            values: ["1", "2", "3"],
+            type: "flattened",
+        }
+        wrapper = shallowMount(List, { propsData })
+    })
+
+    it("renders list when passed", () => {
+        expect(wrapper.find(".list__item:nth-child(2)").text()).toMatch("2,")
+    })
+})
