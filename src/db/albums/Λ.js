2a1211a1b946876adf8669284aa13ea7c6cb4cc1
Benjent <Benjent@hotmail.fr>
2023-09-24 13:04:43 +0200

    feat(db): add stavros logaridis album

--- src/db/albums/Λ.js
+++ src/db/albums/Λ.js
@@ -17,6 +17,21 @@ const albums = [
         similarAlbums: [],
         isAGem: false,
     },
+    {
+        id: "stavros_logaridis_stavros_logaridis",
+        title: "Σταύρος Λογαρίδης",
+        artist: "Σταύρος Λογαρίδης",
+        year: 1978,
+        country: regions.GREECE,
+        criteria: [criteria.GREEK, criteria.JAZZY_GROOVY, criteria.C_ELECTRO, criteria.ODD_RHYTHM, criteria.SOFT_SEVENTIES],
+        selectedTrackTitle: "Σνιφ Σνιφ",
+        selectedTrackYtId: "x5a0ir9JUmk",
+        spotifyId: "40ecy8QFqFBSGXoJKLObDZ",
+        deezerId: "376474157",
+        designers: [],
+        similarAlbums: [],
+        isAGem: false,
+    },
 ]
 
 export default albums
