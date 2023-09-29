a041b46003ab3a096771514ca1d46e2116f51888
Benjent <Benjent@hotmail.fr>
2023-09-29 11:55:16 +0200

    test(Histogram.spec): test threshold and sort props
    - Refactor tests with a wrapperFactory

--- src/components/Histogram/Histogram.spec.js
+++ src/components/Histogram/Histogram.spec.js
@@ -3,22 +3,34 @@ import Transitions from "vue2-transitions"
 import { Histogram } from "@/components"
 
 describe("Histogram.vue", () => {
-    let wrapper
-    beforeEach(() => {
-        const propsData = {
+    function wrapperFactory({ propsData } = {}) {
+        const defaultPropsData = {
             caption: "caption",
             datasource: [{ label: "1", value: 1 }, { label: "2", value: 2 }, { label: "3", value: 3 }],
         }
         const localVue = createLocalVue()
         localVue.use(Transitions)
-        wrapper = shallowMount(Histogram, { propsData, localVue })
-    })
+        return shallowMount(Histogram, { propsData: { ...defaultPropsData, ...propsData }, localVue })
+    }
 
     it("renders caption when passed", () => {
+        const wrapper = wrapperFactory()
         expect(wrapper.find(".histogram__caption").text()).toMatch("caption")
     })
 
     it("renders bar chart", async () => {
+        const wrapper = wrapperFactory()
+        expect(wrapper.find(".histogram__entry:nth-child(1) .histogram__label").text()).toMatch("3")
         expect(wrapper.find(".histogram__entry:nth-child(2) .histogram__label").text()).toMatch("2")
+        expect(wrapper.find(".histogram__entry:nth-child(3) .histogram__label").text()).toMatch("1")
+    })
+
+    it("renders bar chart with ASC and threshold", async () => {
+        const propsData = {
+            sort: "ASC",
+            threshold: 2,
+        }
+        const wrapper = wrapperFactory({ propsData })
+        expect(wrapper.find(".histogram__entry:nth-child(1) .histogram__label").text()).toMatch("2")
     })
 })
