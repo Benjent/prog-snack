9338facaa5e1868cd061504cf3c86a6f224975ee
Benjent <Benjent@hotmail.fr>
2023-11-29 20:06:29 +0100

    docs(vitest.config): add TODO about double-criteria check

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from "vitest/config"
 
 // TODO component testing is broken since the migration from Jest
+// TODO double-criteria check is broken since the migration to vite
 export default defineConfig({
     plugins: [vue()],
     resolve: {
