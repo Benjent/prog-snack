6b72a445883ec293ec8d7c36b1455e5034933b77
Benjent <Benjent@hotmail.fr>
2023-12-01 08:39:12 +0100

    docs(vite-config): add comment about broken tests since vue upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import path from "path"
 import { defineConfig } from "vite"
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
-import vue from '@vitejs/plugin-vue2'
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
+import vue from "@vitejs/plugin-vue2"
 
+// Tests fail because of the following:
+/*
+Error: [vue-test-utils]: window is undefined, vue-test-utils needs to be run in a browser environment.
+You can run the tests in node using jsdom
+*/
+// ... But no jsdom option is present in the vue-test-utils config
 export default defineConfig({
     plugins: [
         vue(),
