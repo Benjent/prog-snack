2ec63e536418fbe22f1e8223743817e439880a1a
Benjent <Benjent@hotmail.fr>
2023-12-09 13:41:13 +0100

    fix(test:unit): make tests run in browser environment
    - some tests are still failing though, but it's still a step forward

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import { defineConfig } from "vite"
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 import vue from "@vitejs/plugin-vue2"
 
-// Tests fail because of the following:
-/*
-Error: [vue-test-utils]: window is undefined, vue-test-utils needs to be run in a browser environment.
-You can run the tests in node using jsdom
-*/
-// ... But no jsdom option is present in the vue-test-utils config
+// Since Vue 2.7 migration, test fails when calling wrapper.emitted() after a custom event trigger
 export default defineConfig({
     plugins: [vue(), ViteImageOptimizer()],
     resolve: {
