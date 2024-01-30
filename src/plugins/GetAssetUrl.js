87f7fd5024cef23402d33d7a9ebc86e6b0035d5b
Benjent <Benjent@hotmail.fr>
2024-01-30 22:10:41 +0100

    fix(GetAssetUrl): use import.meta.blog strategy

--- src/plugins/GetAssetUrl.js
+++ src/plugins/GetAssetUrl.js
@@ -1,5 +1,14 @@
 export default {
     install(Vue) {
-        Vue.prototype.$getAssetUrl = (path) => path && new URL(path, import.meta.url).href
+        // Does not work on production
+        // Vue.prototype.$getAssetUrl = (path) => path && new URL(path, import.meta.url).href
+        Vue.prototype.$getAssetUrl = (path) => {
+            // See "Retrieve items from a Pinia action in Nuxt3" section
+            // https://keith-mifsud.me/blog/build-and-deploy-nuxt3-static-site-with-pinia-and-stripe-checkout-on-firebase
+            const assets = import.meta.glob("/src/assets/**", { eager: true })
+            if (assets[path]) {
+                return assets[path].default
+            }
+        }
     },
 }
